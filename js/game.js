426daa9085ca391b2c1715574d91675c31266236
shoffman <shoffman@pymblelc.nsw.edu.au>
2022-06-21 20:25:40 +1000

    changed html source

--- js/game.js
+++ js/game.js
@@ -120,6 +120,43 @@ var Game = {
     //move counter + every time update runs (per turn), allowing enemy movement to rely on player (turn based playing)
     update: function () {
         this.moveCounter++;
+
+        var display = new ROT.Display({ DisplayOptions });
+        document.getElementById("game").appendChild(display.getContainer());
+
+        // generate map and store its data 
+        var data = {};
+        var map = new ROT.Map.Uniform(width, height);
+        map.create(function (x, y, value) {
+            data[x + "," + y] = value; // data["1,2"]
+            display.DEBUG(x, y, value);
+        });
+
+        // "0" == 0 // true (ducktype)
+        // "0" === 0 // false (actually checking string vs integer) 
+
+        // input callback informs about map structure 
+        var passableCallback = function (x, y) {
+            return (data[x + "," + y] === 0);
+        }
+
+        // prepare path to given coords (target)
+        var astar = new ROT.Path.AStar(Game.enemy.x, Game.enemy.y, passableCallback);
+
+        // compute from given coords #1 
+        astar.compute(8, 45, function (x, y) {
+            display.draw(x, y, "", "", "#800");
+        });
+
+        // compute from given coords #2 
+        astar.compute(130, 8, function (x, y) {
+            display.draw(x, y, "", "", "#800");
+        });
+
+        // highlight 
+        display.draw(8, 45, "", "", "#3f3");
+        display.draw(130, 8, "", "", "#3f3");
+        display.draw(98, 38, "", "", "#f33"); * /
     },
     // clicking restart button resets game
     restart: function () {
--- js/pathfinding.js
+++ js/pathfinding.js
@@ -3,7 +3,7 @@ ROT.RNG.setSeed(12345);
 var display = new ROT.Display({width:w, height:h, fontSize:6});
 document.getElementById("game").appendChild(display.getContainer());
 
-/* generate map and store its data */
+// generate map and store its data 
 var data = {};
 var map = new ROT.Map.Uniform(w, h);
 map.create(function(x, y, value) {
@@ -11,10 +11,10 @@ map.create(function(x, y, value) {
     display.DEBUG(x, y, value);
 });
 
-/* "0" == 0 // true (ducktype)
-"0" === 0 // false (actually checking string vs integer) */ 
+// "0" == 0 // true (ducktype)
+// "0" === 0 // false (actually checking string vs integer) 
 
-/* input callback informs about map structure */
+// input callback informs about map structure 
 var passableCallback = function(x, y) {
     return (data[x+","+y] === 0);
 }
@@ -22,17 +22,17 @@ var passableCallback = function(x, y) {
 /* prepare path to given coords */
 var astar = new ROT.Path.AStar(98, 38, passableCallback);
 
-/* compute from given coords #1 */
+// compute from given coords #1 
 astar.compute(8, 45, function(x, y) {
     display.draw(x, y, "", "", "#800");
 });
 
-/* compute from given coords #2 */
+// compute from given coords #2 
 astar.compute(130, 8, function(x, y) {
     display.draw(x, y, "", "", "#800");
 });
 
-/* highlight */
+// highlight 
 display.draw(8,  45, "", "", "#3f3");
 display.draw(130, 8, "", "", "#3f3");
-display.draw(98, 38, "", "", "#f33");
+display.draw(98, 38, "", "", "#f33"); 
